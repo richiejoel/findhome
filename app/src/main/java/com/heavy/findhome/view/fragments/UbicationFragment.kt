a912eabfc54bdebd5eaf72223dab48ddafdabfee
Richard Joel Garcia <34136964+richiejoel@users.noreply.github.com>
2021-05-15 19:06:08 -0500

    Custom Markers clickable and polyLine

--- app/src/main/java/com/heavy/findhome/view/fragments/UbicationFragment.kt
+++ app/src/main/java/com/heavy/findhome/view/fragments/UbicationFragment.kt
@@ -9,6 +9,7 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.PorterDuff
 import android.location.Location
+import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -26,9 +27,17 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
 import com.heavy.findhome.R
 import com.heavy.findhome.databinding.FragmentUbicationBinding
+import com.heavy.findhome.utils.DataParser
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
 
 
-class UbicationFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
+class UbicationFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener, GoogleMap.OnMapClickListener {
 
     private var _binding: FragmentUbicationBinding? = null
     private val binding get() = _binding!!
@@ -53,6 +62,7 @@ class UbicationFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocation
     private fun mCreateMapFragment(){
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(this)
+
     }
 
     override fun onMapReady(googleMap: GoogleMap?) {
@@ -73,7 +83,12 @@ class UbicationFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocation
         map.setOnMyLocationButtonClickListener(this)
         map.setOnMyLocationClickListener(this)
         mEnableLocation()
+
+        map.setOnMapClickListener(this)
+
+
     }
+
 
     private fun mCreateMarker(){
         //40.780771, -73.979550
@@ -85,7 +100,7 @@ class UbicationFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocation
         val marker2 = MarkerOptions().position(coordinates2).title("Crash!!")
             .icon(BitmapDescriptorFactory.fromBitmap(getMarkerBitmapFromView()))
 
-        val coordinates3 = LatLng(40.797863, -73.967080)
+        val coordinates3 = LatLng(37.401762, -122.085803)
         val marker3 = MarkerOptions().position(coordinates3).title("Crash!!")
             .icon(BitmapDescriptorFactory.fromBitmap(getMarkerBitmapFromView()))
 
@@ -95,6 +110,24 @@ class UbicationFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocation
         map.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 14f), 2000, null)
         map.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinates2, 14f), 800, null)
         map.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinates2, 14f), 1000, null)
+    }
+
+    private fun createPolylines(origin: LatLng, dest: LatLng){
+        val polylineOptions = PolylineOptions()
+            .add(origin)
+            .add(dest)
+            .width(15f)
+            .color(ContextCompat.getColor(requireContext(), R.color.color_map_line))
+
+        val polyline: Polyline = map?.addPolyline(polylineOptions)
+        polyline.startCap = RoundCap()
+        polyline.endCap = RoundCap()
+
+        /*val pattern: List<PatternItem> listOf(
+            Dot(),Gap(10f),Dash(50f),Gap(10f)
+        )
+
+        polyline.pattern(pattern)*/
     }
 
     private fun getMarkerBitmapFromView(): Bitmap? {
@@ -201,5 +234,145 @@ class UbicationFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocation
             "Estas en ${loc.latitude} ${loc.longitude}",
             Toast.LENGTH_SHORT
         ).show()
+
+        //Get position currently
+        val origin = LatLng(loc.latitude,loc.longitude)
+        val dest = LatLng(37.401762, -122.085803)
+
+        getDirectionsURL(origin, dest)
+        createPolylines(origin, dest)
+
+        /*val url: String = getDirectionsURL(origin, dest)!!
+        val downloadTask: DownloadTask = DownloadTask()
+        // Start downloading json data from Google Directions API
+        downloadTask.execute(url)*/
+
+    }
+
+    private fun getDirectionsURL(origin:LatLng, dest:LatLng): String? {
+
+        //Origin of route
+        val strOrigin = "origin="+ origin.latitude +","+ origin.longitude
+
+        //Destination of route
+        val strDest = "destination="+ dest.latitude +","+ dest.longitude;
+
+        //setting transportation mode
+        val mode = "sensor=false"
+
+        //Building parameters to the API Directions
+        val parameters = "$strOrigin&$strDest&$mode"
+
+        //Output format
+        val output = "json"
+        //val key = "AIzaSyCIMIVNvWtIaMBDqPySBjsV-q4h4YwSOjA";
+        val key = "AIzaSyCASsBISqXNR4imAynxnIw8Fa68Cv1c8cY";
+
+        //Building the url for API Directions
+        val a = "https://maps.googleapis.com/maps/api/directions/$output?$parameters&key=$key"
+        Log.i("prueba",a)
+        return "https://maps.googleapis.com/maps/api/directions/$output?$parameters&key=$key"
+    }
+
+
+
+    inner class DownloadTask: AsyncTask<String?, Void?, String>(){
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            val parserTask= ParserTask()
+            parserTask.execute(result)
+        }
+
+        override fun doInBackground(vararg url: String?): String {
+            var data = ""
+            try {
+                data = downloadUrl(url[0].toString()).toString()
+            } catch (e: java.lang.Exception){
+                Log.d("Background Task", e.toString())
+            }
+
+            return data
+        }
+
+    }
+
+    @Throws(IOException::class)
+    private fun downloadUrl(strUrl: String): String? {
+        var data = ""
+        var iStream: InputStream? = null
+        var urlConnection: HttpURLConnection? = null
+
+        try {
+            val url = URL(strUrl)
+            urlConnection = url.openConnection() as HttpURLConnection
+            urlConnection.connect()
+            iStream = urlConnection!!.inputStream
+            val br = BufferedReader(InputStreamReader(iStream))
+            val sb = StringBuffer()
+            var line:String? = ""
+            while (br.readLine().also { line = it } != null){
+                sb.append(line)
+            }
+            data = sb.toString()
+            br.close()
+
+        } catch (e: java.lang.Exception){
+            Log.d("Download Url ", e.toString())
+        } finally {
+            iStream!!.close()
+            urlConnection!!.disconnect()
+        }
+
+        return data
+    }
+
+    inner class ParserTask: AsyncTask<String?, Int?, List<List<HashMap<String,String>>>?> (){
+
+        override fun doInBackground(vararg jsonData: String?): List<List<HashMap<String, String>>>? {
+            val jsonObject: JSONObject
+            var routes: List<List<HashMap<String, String>>>? = null
+            try {
+                jsonObject = JSONObject(jsonData[0])
+                val parser = DataParser()
+                routes = parser.parse(jsonObject)
+            } catch (e: java.lang.Exception){
+                e.printStackTrace()
+            }
+
+            return routes
+        }
+
+        override fun onPostExecute(result: List<List<HashMap<String, String>>>?) {
+            super.onPostExecute(result)
+            val points = ArrayList<LatLng?>()
+            val lineOptions = PolylineOptions()
+
+            for(i in result!!.indices){
+                val path = result[i]
+                for(j in path!!.indices){
+                    val point = path[j]
+                    val lat = point["lat"]!!.toDouble()
+                    val lng = point["lng"]!!.toDouble()
+                    val position = LatLng(lat, lng)
+                    points.add(position)
+                }
+                lineOptions.addAll(points)
+                lineOptions.width(8f)
+                lineOptions.color(Color.RED)
+                lineOptions.geodesic(true)
+            }
+
+            //Drawing polyline in google map
+            if(points.size != 0) map!!.addPolyline(lineOptions)
+        }
+
+    }
+
+    override fun onMapClick(position: LatLng?) {
+        val markerAdd = MarkerOptions()?.position(position!!).title("Crash!!")
+            .icon(BitmapDescriptorFactory.fromBitmap(getMarkerBitmapFromView()))
+
+        map?.addMarker(markerAdd)
     }
 }
